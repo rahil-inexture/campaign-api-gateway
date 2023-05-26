70f0754e17b0e0217d21b3ee4aaee91dea896b36
rahil-inexture <rahil@inexture.com>
2023-05-26 11:27:15 +0530

    first commit

---
+++ src/test/java/com/campaign/gateway/CampaignApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campaign.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
